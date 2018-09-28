ff91a1d8ba9fca6c3157ff3e72f0f283f2a1acac
Mavis <angel253698@gmail.com>
2018-09-29 00:19:33 +0800

    SelectionSort06

--- src/com/mavis/hw001/ExchangeSort.java
+++ src/com/mavis/hw001/ExchangeSort.java
@@ -18,24 +18,18 @@ public class ExchangeSort {
 		int i, j;
 		int min, temp;
 		for (i = 0; i < randomnumber.length; i++) {
-			min = i;
 			for (j = i + 1; j < randomnumber.length; j++)
 				if (randomnumber[i] > randomnumber[j]) {
-					min = j;
 					temp = randomnumber[i];
 					randomnumber[i] = randomnumber[j];
 					randomnumber[j] = temp;
 				}
 		}
+		
 		int y = 0;
 		for(int x = 0 ; x < randomnumber.length; x++) {
 			 y = randomnumber[x];
 			 System.out.print( y + " ");
 		}
-		
-		
-		
 	}
-	
-
 }
--- src/com/mavis/hw001/SelectionSort.java
+++ src/com/mavis/hw001/SelectionSort.java
@@ -10,34 +10,31 @@ public class SelectionSort {
 
 		Random r = new Random();
 		int randomnumber[] = new int[10];
-
 		for (int i = 0; i < 10; i++) {
 			randomnumber[i] = (int) r.nextInt((100) + 1);
 		}
 
-		int i, j;
 		int min, temp;
-		for (i = 0; i < randomnumber.length; i++) {
+		for (int i = 0; i < randomnumber.length - 1; i++) {
 			min = i;
-			for (j = i; j < randomnumber.length; j++)
+			for (int j = i + 1; j < randomnumber.length; j++) {
 				if (randomnumber[i] < randomnumber[j]) {
 					min = j;
-				}else {
-					break;
-				}
-			temp = randomnumber[min] ;
-			 randomnumber[min] =  randomnumber[i];
-			 randomnumber[i] = temp;
-			
-			
-		}
-		for(int x = 0 ; x < randomnumber.length; x++) {
-			 System.out.print( y + " ");
-		}
-		
-		
-		
-	}
-	
 
+					if (i != min) {
+						temp = randomnumber[i];
+						randomnumber[i] = randomnumber[min];
+						randomnumber[min] = temp;
+					}
+				}
+			}
+//			temp = randomnumber[min];
+//			randomnumber[min] = randomnumber[i];
+//			randomnumber[i] = temp;
+		}
+
+		for (int x = 0; x < randomnumber.length; x++) {
+			System.out.print(randomnumber[x] + " ");
+		}
+	}
 }
