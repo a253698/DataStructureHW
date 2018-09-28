c1e15c2ce5dd18bf6f4cf6c0fe1c1ba3cd69db7b
Mavis <angel253698@gmail.com>
2018-09-28 23:29:22 +0800

    SelectionSort03

--- src/com/mavis/hw001/SelectionSort.java
+++ src/com/mavis/hw001/SelectionSort.java
@@ -12,12 +12,11 @@ public class SelectionSort {
 		int randomnumber[] = new int[10];
 
 		for (int i = 0; i < 10; i++) {
-			randomnumber[i] = (int) r.nextInt((20) + 1);
+			randomnumber[i] = (int) r.nextInt((100) + 1);
 		}
 
 		int i, j;
 		int min, temp;
-		int y = 0;
 		for (i = 0; i < randomnumber.length; i++) {
 			min = i;
 			for (j = i + 1; j < randomnumber.length; j++)
@@ -28,6 +27,7 @@ public class SelectionSort {
 					randomnumber[j] = temp;
 				}
 		}
+		int y = 0;
 		for(int x = 0 ; x < randomnumber.length; x++) {
 			 y = randomnumber[x];
 			 System.out.print( y + " ");
