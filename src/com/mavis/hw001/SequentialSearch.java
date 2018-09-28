a702ff785b5ded45bd27c7cd2c803698281360f8
Mavis <angel253698@gmail.com>
2018-09-29 01:29:09 +0800

    SequentialSearch01

--- src/com/mavis/hw001/SequentialSearch.java
+++ src/com/mavis/hw001/SequentialSearch.java
@@ -1,10 +1,22 @@
 package com.mavis.hw001;
 
+import java.util.Random;
+
 public class SequentialSearch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Random r = new Random();
+		int randomnumber[] = new int[10];
+
+		for (int h = 0; h < 10; h++) {
+			randomnumber[h] = (int) r.nextInt((100) + 1);
+		}
+		
+		for (int x = 0; x < randomnumber.length; x++) {
+			System.out.print(randomnumber[x] + " ");
+		}
+		
 	}
 
 }
