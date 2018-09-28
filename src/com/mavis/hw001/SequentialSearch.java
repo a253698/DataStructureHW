dab13fe56156a525a8bd5c819c2c42513f2a6cb6
Mavis <angel253698@gmail.com>
2018-09-29 01:35:40 +0800

    SequentialSearch02

--- src/com/mavis/hw001/SequentialSearch.java
+++ src/com/mavis/hw001/SequentialSearch.java
@@ -1,6 +1,7 @@
 package com.mavis.hw001;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class SequentialSearch {
 
@@ -8,14 +9,33 @@ public class SequentialSearch {
 
 		Random r = new Random();
 		int randomnumber[] = new int[10];
-
 		for (int h = 0; h < 10; h++) {
 			randomnumber[h] = (int) r.nextInt((100) + 1);
 		}
 		
+		System.out.print("Number you are searching：");
+		Scanner scanner = new Scanner(System.in);
+		int searchingnumber = scanner.nextInt();
+		
+		
+		int position = 0;
+		for (int i = 0; i < randomnumber.length; i++) {
+			if (searchingnumber == i) {
+				boolean correct = true;
+				position = i;
+				break;
+			}
+		}
+		
+
+
+		
+		
 		for (int x = 0; x < randomnumber.length; x++) {
 			System.out.print(randomnumber[x] + " ");
 		}
+		
+		
 		
 	}
 
