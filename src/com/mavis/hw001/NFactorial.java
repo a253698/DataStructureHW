09eb5779928fa278f0d67a1510be49b82b4b290c
Mavis <angel253698@gmail.com>
2018-09-29 01:02:36 +0800

    NFactorial01

--- src/com/mavis/hw001/NFactorial.java
+++ src/com/mavis/hw001/NFactorial.java
@@ -1,10 +1,39 @@
 package com.mavis.hw001;
 
+import java.util.Scanner;
+
 public class NFactorial {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	
+	Scanner scanner = new Scanner(System.in);
+//	int type = scanner.nextInt();
+	
+	public NFactorial() {
 	}
 
+	public static void main(String[] args) {
+		 NFactorial nFactorial = new NFactorial();
+		 nFactorial.ask();
+		 nFactorial.calculator();
+	}
+	
+	public void ask() {
+		System.out.println("請輸入階層數：");
+//		Scanner scanner = new Scanner(System.in);
+//		int type = scanner.nextInt();
+		
+	}
+	
+	public void calculator(){
+		int type = scanner.nextInt();
+		int finalans = 0;
+		if(type != 0) {
+			for(int a = type; a >= 1 ; a--) {
+				finalans = finalans * a;
+			}
+		}
+		System.out.println(finalans);
+	}
+	
+	
 }
