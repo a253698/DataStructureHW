8d460c23e3db7bcce665d31960bc10545c36b4f6
Mavis <angel253698@gmail.com>
2018-09-29 16:37:32 +0800

    TwoDimensionalArray003

--- src/com/mavis/hw001/TwoDimensionalArray.java
+++ src/com/mavis/hw001/TwoDimensionalArray.java
@@ -15,11 +15,11 @@ public class TwoDimensionalArray {
 	public static void main(String[] args) {
 		int A[][] = { { 1, 2 }, { 5, 6 } };
 		int B[][] = { { 3, 4 }, { 7, 8 } };
-		System.out.println("A matrix:");
-		print(A);
-		System.out.println("B matrix:");
-		print(B);
-		System.out.println("C matrix:");
+//		System.out.println("A matrix:");
+//		print(A);
+//		System.out.println("B matrix:");
+//		print(B);
+		System.out.println("New matrix:");
 		int C[][] = Mul(A, B);
 		print(C);
 	}
@@ -37,8 +37,11 @@ public class TwoDimensionalArray {
 		int[][] C = new int[A.length][B[0].length];
 
 		for (int i = 0; i < C.length; i++) {
+			// 利用A的row定義新矩陣的row
 			for (int j = 0; j < C[i].length; j++) {
+				// 利用B的col定義新矩陣的col
 				for (int k = 0; k < A[0].length; k++) {
+					
 					C[i][j] += A[i][k] * B[k][j];
 				}
 			}
